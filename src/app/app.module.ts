0028df4c6bfeb9e03b78a885ce6534397c6a8eee
Guillaume BADINIER <g.badinier@hotmail.fr>
2018-07-20 16:08:25 +0200

    Adding EventService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,9 @@ import { NavBarComponent } from './nav/navbar.component';
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent
+  ],
+  providers: [
+    EventService
   ],
   imports: [
     BrowserModule
